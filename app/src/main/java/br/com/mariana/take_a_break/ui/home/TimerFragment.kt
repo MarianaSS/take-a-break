e2267a91eb14c10d03242e01c3e2ac1559b8af2d
Mariana Santana <sansilmari2@gmail.com>
2020-09-24 22:52:39 -0300

    Adiciona completude do timer

--- app/src/main/java/br/com/mariana/take_a_break/ui/home/TimerFragment.kt
+++ app/src/main/java/br/com/mariana/take_a_break/ui/home/TimerFragment.kt
@@ -1,16 +1,19 @@
 package br.com.mariana.take_a_break.ui.home
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import br.com.mariana.take_a_break.R
+import kotlinx.android.synthetic.main.fragment_timer.*
 
 class TimerFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private var countDownTimer: CountDownTimer? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -20,5 +23,45 @@ class TimerFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         return inflater.inflate(R.layout.fragment_timer, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        startTimer()
+        buttonPauseTimer.setOnClickListener(onClickListener)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        cancelTimer()
+    }
+
+    private val onClickListener = View.OnClickListener {
+        when (it.id) {
+            R.id.buttonPauseTimer -> {
+                cancelTimer()
+            }
+        }
+    }
+
+    private fun startTimer() {
+        countDownTimer = object : CountDownTimer(100000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val progress = (millisUntilFinished/1000).toInt()
+                progressBar.progress = progressBar.max - progress
+                countDown.start()
+
+            }
+            override fun onFinish() {
+                cancelTimer()
+            }
+        }
+        (countDownTimer as CountDownTimer).start()
+    }
+
+    private fun cancelTimer() {
+        if (countDownTimer != null) countDownTimer!!.cancel()
+        countDown.stop()
     }
 }
