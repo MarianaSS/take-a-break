c286fa4c08699cdb5769b4894cc3df0f8fc15727
Mariana Santana <sansilmari2@gmail.com>
2020-09-25 01:00:26 -0300

    add funcao de play e pause ao timer

--- app/src/main/java/br/com/mariana/take_a_break/ui/home/TimerFragment.kt
+++ app/src/main/java/br/com/mariana/take_a_break/ui/home/TimerFragment.kt
@@ -2,6 +2,7 @@ package br.com.mariana.take_a_break.ui.home
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.SystemClock
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,21 +15,24 @@ class TimerFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var countDownTimer: CountDownTimer? = null
+    private var timerProgress: Long = 0
+    private var counterProgress: Long = 0
+    private var isPaused: Boolean = false
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
         return inflater.inflate(R.layout.fragment_timer, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        startTimer()
+        startTimer(100000)
         buttonPauseTimer.setOnClickListener(onClickListener)
     }
 
@@ -38,23 +42,49 @@ class TimerFragment : Fragment() {
     }
 
     private val onClickListener = View.OnClickListener {
-        when (it.id) {
-            R.id.buttonPauseTimer -> {
-                cancelTimer()
-            }
+        isPaused = if (isPaused) {
+            setTimerAsRunning()
+        } else {
+            setTimerAsPaused()
         }
     }
 
-    private fun startTimer() {
-        countDownTimer = object : CountDownTimer(100000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                val progress = (millisUntilFinished/1000).toInt()
-                progressBar.progress = progressBar.max - progress
-                countDown.start()
+    private fun setTimerAsPaused(): Boolean {
+        buttonPauseTimer.setImageDrawable(
+            resources.getDrawable(
+                R.drawable.ic_baseline_play_arrow_24,
+                context?.theme
+            )
+        )
+        counterProgress = countDown.base - SystemClock.elapsedRealtime()
+        cancelTimer()
+        return true
+    }
 
+    private fun setTimerAsRunning(): Boolean {
+        buttonPauseTimer.setImageDrawable(
+            resources.getDrawable(
+                R.drawable.ic_baseline_pause_24,
+                context?.theme
+            )
+        )
+        countDown.base = SystemClock.elapsedRealtime() + counterProgress
+        startTimer(timerProgress)
+        return false
+    }
+
+    private fun startTimer(millisInFuture: Long) {
+        countDownTimer = object : CountDownTimer(millisInFuture, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val progress = (millisUntilFinished / 1000).toInt()
+                progressBar.progress = progressBar.max - progress
+                timerProgress = millisUntilFinished
+                countDown.start()
             }
+
             override fun onFinish() {
-                cancelTimer()
+                setTimerAsPaused()
+                buttonPauseTimer.isEnabled = false
             }
         }
         (countDownTimer as CountDownTimer).start()
