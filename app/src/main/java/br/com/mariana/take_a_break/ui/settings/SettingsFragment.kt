90fd128dff80897b567c1846ccc37d9eb93fc4d9
Mariana Santana <sansilmari2@gmail.com>
2020-12-21 00:41:00 -0200

    Seta componente proxima tarefa

--- app/src/main/java/br/com/mariana/take_a_break/ui/settings/SettingsFragment.kt
+++ app/src/main/java/br/com/mariana/take_a_break/ui/settings/SettingsFragment.kt
@@ -1,14 +1,57 @@
 package br.com.mariana.take_a_break.ui.settings
 
 import android.os.Bundle
+import androidx.fragment.app.viewModels
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import androidx.preference.SeekBarPreference
 import br.com.mariana.take_a_break.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
-    private lateinit var settingsViewModel: SettingsViewModel
+    private val settingsViewModel: SettingsViewModel by viewModels()
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+        retainInstance = true
+        findPreference<Preference>("trabalho")?.let { bindPreferenceSummaryToValue(it) }
+        findPreference<Preference>("pausa")?.let { bindPreferenceSummaryToValue(it) }
+
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        settingsViewModel.updateValue(prefs.getInt("trabalho", 25))
+    }
+
+    companion object {
+
+        /**
+         * A preference value change listener that updates the preference's summary
+         * to reflect its new value.
+         */
+        private val sBindPreferenceSummaryToValueListener =
+            Preference.OnPreferenceChangeListener { preference, value ->
+
+                val stringValue = value.toString()
+
+                if (preference is SeekBarPreference) {
+                    preference.summary = stringValue
+                }
+
+                true
+            }
+
+        private fun bindPreferenceSummaryToValue(preference: Preference) {
+            // Set the listener to watch for value changes.
+            preference.onPreferenceChangeListener = sBindPreferenceSummaryToValueListener
+
+            // Trigger the listener immediately with the preference's
+            // current value.
+            sBindPreferenceSummaryToValueListener.onPreferenceChange(
+                preference,
+                PreferenceManager
+                    .getDefaultSharedPreferences(preference.context)
+                    .getInt(preference.key, 25)
+            )
+        }
     }
 }
