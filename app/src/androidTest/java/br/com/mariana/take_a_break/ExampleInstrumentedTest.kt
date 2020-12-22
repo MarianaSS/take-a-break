10ad6d3fe043ad13387e515177cca3333cfe82ac
tpsouza <40897828+tpsouza@users.noreply.github.com>
2020-12-22 20:47:31 -0300

    chore: change test example

--- app/src/androidTest/java/br/com/mariana/take_a_break/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/br/com/mariana/take_a_break/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("br.com.mariana.take_a_break", appContext.packageName)
+        assertEquals("test", "test")
     }
 }
